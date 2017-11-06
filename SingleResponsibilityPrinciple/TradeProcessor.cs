dc6b026593ab87bc2d72b55d7eb9f14674ebad72
Amanda Nichols <anichols1@css.edu>
2017-11-05 20:30:38 -0600

    Request  407: Created URLTradeDataProcessor to add the trade URL reader and called in in the main program. Also created IURLTradeDataProvider interface to store GetUrlTradeData method from URLTradeDataProcessor.

--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -1,24 +1,34 @@
 ﻿
+using SingleResponsibilityPrinciple.AdoNet;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
     public class TradeProcessor
     {
-        public TradeProcessor(ITradeDataProvider tradeDataProvider, ITradeParser tradeParser, ITradeStorage tradeStorage)
+        public TradeProcessor(IURLTradeDataProvider uRLTradeDataProvider, ITradeDataProvider tradeDataProvider, ITradeParser tradeParser, AdoNetTradeStorage tradeStorage)
         {
+            this.uRLTradeDataProvider = uRLTradeDataProvider;
             this.tradeDataProvider = tradeDataProvider;
+            this.tradeParser = tradeParser;
+            this.tradeStorage = tradeStorage;
+        }
+
+        public TradeProcessor(URLTradeDataProvider uRLTradeDataProvider, SimpleTradeParser tradeParser, AdoNetTradeStorage tradeStorage)
+        {
+            this.uRLTradeDataProvider = uRLTradeDataProvider;
             this.tradeParser = tradeParser;
             this.tradeStorage = tradeStorage;
         }
 
         public void ProcessTrades()
         {
-            var lines = tradeDataProvider.GetTradeData();
+            var lines = uRLTradeDataProvider.GetURLTradeData();
             var trades = tradeParser.Parse(lines);
             tradeStorage.Persist(trades);
         }
 
+        private readonly IURLTradeDataProvider uRLTradeDataProvider;
         private readonly ITradeDataProvider tradeDataProvider;
         private readonly ITradeParser tradeParser;
         private readonly ITradeStorage tradeStorage;
---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using SingleResponsibilityPrinciple.Contracts;
+using System.Net;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class URLTradeDataProvider : IURLTradeDataProvider
+    {
+        public URLTradeDataProvider(string url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<string> GetURLTradeData()
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+
+        }
+
+        private readonly string url;
+    }
+}
